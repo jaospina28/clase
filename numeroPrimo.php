26b37ce570a566d5684d3fb0552191bd6127967b
jaospina28 <jhonny.ospina@uniremingtonmanizales.edu.co>
2017-03-14 21:27:41 -0500

    Se añade comentarios al codigo

--- numeroPrimo.php
+++ numeroPrimo.php
@@ -5,15 +5,18 @@
 	 $j=0;
 	 $esPrimo=0; 
 	
+	//for para recorrer los numeros primos del 1 al 100
 	for($i = 2; $i <= 100; $i++) 
      { 
          $esPrimo = 1; 
+		 //segundo for
           for($j = 2; $j < $i; $j ++) {
                if(!($i % $j)) 
                { 
                     $esPrimo = 0;  
                } 
           }
+		  //si es el numero es primo se imprime en pantalla
 		  if($esPrimo) {
                echo $i."<br>"; 
 	      }
